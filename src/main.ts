db47ecca465472a13ad5f89d9a3f8e88706a43fb
Aguru Itabashi <itabashi.a@gmail.com>
2025-02-12 15:53:54 +0900

    main.tsを修正

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+})
 
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app')
